5723eeafc1526c10befbc1c13cd4487365c661e2
YasinBedirhanSimsek <yasinbedirhansimseq@gmail.com>
2021-04-23 12:55:46 +0300

    Push Test 1

--- MIPS/MIPS/Form1.cs
+++ MIPS/MIPS/Form1.cs
@@ -16,5 +16,10 @@ namespace MIPS
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("LaĞ PaTLiyAcİĞİ");
+        }
     }
 }
